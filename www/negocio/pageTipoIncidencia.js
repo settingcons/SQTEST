b93fc89fc630e18bb2f8ca9b70e98f26718c2f9c
settingcons <settingcons@gmail.com>
2015-07-16 13:05:12 +0200

    20150716_5
    Reactivant camera i canviant el nom

--- www/negocio/pageTipoIncidencia.js
+++ www/negocio/pageTipoIncidencia.js
@@ -78,7 +78,7 @@ function mostrarImagenes() {
 function selectTipo(p_tipo) {
     try{
         TipoInciSel = p_tipo;
-       //hgs150715 descomentar  navigator.camera.getPicture(hacerfotoOK, hacerFotoERROR, { quality: 10, destinationType: Camera.DestinationType.DATA_URL, correctOrientation: true,sourceType:  Camera.PictureSourceType.CAMERA,  saveToPhotoAlbum: false });
+       navigator.camera.getPicture(hacerfotoOK, hacerFotoERROR, { quality: 10, destinationType: Camera.DestinationType.DATA_URL, correctOrientation: true,sourceType:  Camera.PictureSourceType.CAMERA,  saveToPhotoAlbum: false });
         abrirPagina('pageDatosIncidencia', false);
     }
     catch (ex){
