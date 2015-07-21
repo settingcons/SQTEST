a5dd64a13d8bfeeb73a9d7ea98ef0578e5725063
settingcons <settingcons@gmail.com>
2015-07-21 13:07:30 +0200

    20150721

--- www/negocio/variables.js
+++ www/negocio/variables.js
@@ -1,23 +1,20 @@
 //Coordenadas plaza de la vila
-//var _ayuntamiento_posicionX = '41,470480';
-//var _ayuntamiento_posicionY = '2,0852293';
-var _ayuntamiento_posicionX = '423713';
-var _ayuntamiento_posicionY = '4591617';
+//hay que controlar formato de las coordenadas aqui?
+var _ayuntamiento_posicionX = '41,470480';
+var _ayuntamiento_posicionY = '2,0852293';
 
 
 //dmz
-
 //testvar _wsURLCrearIncidencia="http://80.39.72.44:8000/wsAPPGIV/wsIncidentNotifierGIV.asmx/IncidenciaTipus_v1";
-//var _wsURLCrearIncidencia="http://80.39.72.44:8000/wsAPPSQ/wsAPPSQ.asmx/TestWs";
-
+//testvar _wsURLCrearIncidencia="http://80.39.72.44:8000/wsAPPSQ/wsAPPSQ.asmx/TestWs";
 var _wsURLCrearIncidencia="http://80.39.72.44:8000/wsAPPSQ/wsAPPSQ.asmx/IncidenciaTipus";
 var _wsURLConsultaIncidencia="http://80.39.72.44:8000/wsAPPSQ/wsAPPSQ.asmx/ConsultaComunicatsTipus";
 var _wsURLLogin="http://80.39.72.44:8000/wsAPPSQ/wsAPPSQ.asmx/Login";
 
 //Santcugat
-//var _wsURLCrearIncidencia="http://www.vilafranca.cat/wsAPPSQ/wsAPPSQ.asmx/IncidenciaTipus";
-//var _wsURLConsultaIncidencia="http://www.vilafranca.cat/wsAPPSQ/wsAPPSQ.asmx/ConsultaComunicatsTipus";
-//var _wsURLLogin="http://www.vilafranca.cat/wsAPPSQ/wsAPPSQ.asmx/Login";
+//var _wsURLCrearIncidencia="http://82.159.148.100:81/wsAPPSQ/wsAPPSQ.asmx/IncidenciaTipus";
+//var _wsURLConsultaIncidencia="http://82.159.148.100:81/wsAPPSQ/wsAPPSQ.asmx/ConsultaComunicatsTipus";
+//var _wsURLLogin="http://82.159.148.100:81/wsAPPSQ/wsAPPSQ.asmx/Login";
 
 
 var _mediaAudio;
